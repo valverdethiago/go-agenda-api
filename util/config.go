a42634f8de77cca625f3aed565c43eab9ba01697
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2021-03-11 10:36:35 -0300

    Adding configuration to the app

---
+++ util/config.go
@@ -0,0 +1,23 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBServer      string `mapstructure:"DB_SERVER"`
+	DBName        string `mapstructure:"DB_NAME"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string, env string) (Config, error) {
+	var config Config
+	viper.AddConfigPath(path + "/env")
+	viper.SetConfigName(env)
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		return config, err
+	}
+	err = viper.Unmarshal(&config)
+	return config, err
+}
